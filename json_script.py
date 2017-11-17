fe426ecca9ba0c4919d8ba16ff3c0c02571ba8f3
chgad <chgad.games@gmail.com>
2017-11-17 13:31:47 +0100

    had an saving problem and a bit of corruption eliminating

--- json_script.py
+++ json_script.py
@@ -1,22 +1,38 @@
 import json
 
-container = []
+# This skript is made to parse a file containing a JSON Array of objects like
+#this :
+#       {"header": <string>,
+#        "description":<string>,
+#        "id":<string>}
+#To a form like the dummy_object where the fields are description, header and
+#examples.
 
-dummy_object = {
-        "model":"stein.glossaryentry",
-        "pk":"",
-        "fields":{}
-        }
-
-with open("test_json.json") as json_data:
+with open("glossary_json.json") as json_data:
     d = json.load(json_data)
 
+container = list()
+
 for obj in d:
-    new = dummy_object
+    new = dummy_object = {
+        "model": "stein.glossaryentry",
+        "pk": "",
+        "fields": {}
+        }
+
     new["pk"] = obj["id"]
     del obj["id"]
+    if "list" in obj:
+        del obj["list"]
+
     new["fields"] = obj
+    if "examples" in obj:
+        new["fields"]["examples"] = obj["examples"]
     container.append(new)
 
-print(container)
-    
+ret = json.dumps(container)
+
+with open("glossary_16_11_2017.json", "w") as file:
+    file.write(ret)
+
+
